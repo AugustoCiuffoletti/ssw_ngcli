238b32b30bd9e2b19e3b934220a63bb6489d17f4
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2023-05-09 08:28:53 +0000

    Commentato strict, sistemato weather e root ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TempCity } from './temp-city';
+import { WeatherService } from './weather.service';
+import { AjaxResponse } from 'rxjs/ajax';
+import { NotificaComponent } from './notifica/notifica.component';
+import { NewcityComponent } from './newcity/newcity.component';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +12,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   standalone: true
 })
-export class AppComponent {
-  title = 'ngtemperature';
+
+export class AppComponent implements OnInit {
+  title: string = 'Temperature in Angular ' + VERSION.major;
+  cities: Array<string> = ['Torino', 'Milano', 'Genova'];
+
+  seleziona(name: string) {
+    this.selezione = new TempCity(name, undefined);
+    this.ws.getData(this.selezione.nome).subscribe({
+      next: (x: AjaxResponse<any>) =>
+        (this.selezione.valore = x.response.main.temp),
+      error: (err) =>
+        console.error('Observer got an error: ' + JSON.stringify(err)),
+    });
+  }
+  selezione: TempCity;
+  addCity(newCity: string) {
+    this.cities.push(newCity);
+  }
+
+  constructor(private ws: WeatherService) {}
+  ngOnInit() {}
 }
